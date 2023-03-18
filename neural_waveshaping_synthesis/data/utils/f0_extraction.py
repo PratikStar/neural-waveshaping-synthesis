2a186b7382c4b17eb9f9857baea7e3ec93170b61
Pratik Sutar <sutarpratik2012@gmail.com>
2023-03-18 23:17:12 +0900

    development

--- neural_waveshaping_synthesis/data/utils/f0_extraction.py
+++ neural_waveshaping_synthesis/data/utils/f0_extraction.py
@@ -66,79 +66,31 @@ def resample_audio_dupli(audio: np.ndarray, original_sr: float, target_sr: float
 
 @gin.configurable
 def extract_f0_with_crepe(
-    audio: np.ndarray,
-    file: str,
-    sample_rate: float,
-    hop_length: int = 128,
-    minimum_frequency: float = 50.0,
-    maximum_frequency: float = 2000.0,
-    full_model: bool = True,
-    batch_size: int = 2048,
-    device: Union[str, torch.device] = "cpu",
-    interpolate_fn: Optional[Callable] = linear_interpolation,
-    f0_from_di: bool = False,
-    normalisation_factor: Union[float, None] = None,
-    target_sr: float = 16000.0,
+        audio: np.ndarray,
+        sample_rate: float,
+        hop_length: int = 128,
+        minimum_frequency: float = 50.0,
+        maximum_frequency: float = 2000.0,
+        full_model: bool = True,
+        batch_size: int = 2048,
+        device: Union[str, torch.device] = "cpu",
+        interpolate_fn: Optional[Callable] = linear_interpolation,
 ):
     # convert to torch tensor with channel dimension (necessary for CREPE)
     audio = torch.tensor(audio).unsqueeze(0)
-
-    if f0_from_di:
-        print("Getting f0 estimate from DI")
-        file = Path(file)
-
-        di_filename = f"09A DI - {file.name.split()[-1].split('.')[0]}.wav"
-        di_path = file.parent / di_filename
-        if not di_path.exists():
-            raise Exception(f"DI not found at {di_path}")
-
-
-        if di_filename in di_f0_estimates:
-            f0, confidence = di_f0_estimates[di_filename]
-        else:
-            print("\nLoading DI file: %s..." % di_path)
-            original_sr, di_audio = wavfile.read(di_path)
-            di_audio = convert_to_float32_audio_dupli(di_audio)
-            di_audio = make_monophonic_dupli(di_audio)
-
-            if normalisation_factor:
-                di_audio = normalise_signal_dupli(di_audio, normalisation_factor)
-
-            print("Resampling audio file: %s..." % di_path)
-            print(f"audio.shape: {di_audio.shape}")
-
-            di_audio = resample_audio_dupli(di_audio, original_sr, target_sr)
-
-            print("Extracting DI f0")
-            f0, confidence = torchcrepe.predict(
-                di_audio,
-                sample_rate,
-                hop_length,
-                minimum_frequency,
-                maximum_frequency,
-                "full" if full_model else "tiny",
-                batch_size=batch_size,
-                device=device,
-                decoder=torchcrepe.decode.viterbi,
-                # decoder=torchcrepe.decode.weighted_argmax,
-                return_harmonicity=True,
-            )
-            di_f0_estimates[di_filename] = [f0, confidence]
-
-    else:
-        f0, confidence = torchcrepe.predict(
-            audio,
-            sample_rate,
-            hop_length,
-            minimum_frequency,
-            maximum_frequency,
-            "full" if full_model else "tiny",
-            batch_size=batch_size,
-            device=device,
-            decoder=torchcrepe.decode.viterbi,
-            # decoder=torchcrepe.decode.weighted_argmax,
-            return_harmonicity=True,
-        )
+    f0, confidence = torchcrepe.predict(
+        audio,
+        sample_rate,
+        hop_length,
+        minimum_frequency,
+        maximum_frequency,
+        "full" if full_model else "tiny",
+        batch_size=batch_size,
+        device=device,
+        decoder=torchcrepe.decode.viterbi,
+        # decoder=torchcrepe.decode.weighted_argmax,
+        return_harmonicity=True,
+    )
 
     f0, confidence = f0.squeeze().numpy(), confidence.squeeze().numpy()
 
