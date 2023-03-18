c63377ee5cf25625b5d7e1850f31da5fa72d7539
Pratik Sutar <sutarpratik2012@gmail.com>
2023-03-18 17:24:23 +0900

    development

--- neural_waveshaping_synthesis/data/utils/f0_extraction.py
+++ neural_waveshaping_synthesis/data/utils/f0_extraction.py
@@ -37,11 +37,7 @@ def extract_f0_with_crepe(
     if f0_from_di:
         print("Getting f0 estimate from DI")
         file = Path(file)
-        print(file.name)
-        print(file.name.split())
-        print(file.name.split()[-1])
-        print(file.name.split()[-1].split('.'))
-        print(file.name.split()[-1].split('.')[0])
+
         di_filename = f"09A DI - {file.name.split()[-1].split('.')[0]}.wav"
         di_path = file.parent / di_filename
         if not di_path.exists():
