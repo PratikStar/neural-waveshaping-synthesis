c911041c694541f56fb886239a80882b41b3ccdb
Pratik Sutar <sutarpratik2012@gmail.com>
2023-03-18 17:06:47 +0900

    development

--- wisteria/interactive.sh
+++ wisteria/interactive.sh
@@ -18,6 +18,5 @@ echo "======================"
 python scripts/create_dataset.py \
 --gin-file gin/data/urmp_4second_crepe.gin \
 --data-directory /work/gk77/k77021/data/timbre/monophonic-4secchunks \
---output-directory /work/gk77/k77021/nws/monophonic-4secchunks \
+--output-directory /work/gk77/k77021/nws/monophonic-4secchunks-di_f0 \
 --device cuda:0
-
