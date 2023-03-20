bc19d7733e59d95686c584beea5baa56285feafd
Pratik Sutar <sutarpratik2012@gmail.com>
2023-03-21 02:57:37 +0900

    development

--- scripts/train.py
+++ scripts/train.py
@@ -69,7 +69,6 @@ def main(
         logger = pl.loggers.WandbLogger(project="nws")
         logger.watch(model, log="all")
 
-    print(f"data: {len(data.urmp_train)}")
     kwargs = trainer_kwargs()
     trainer = pl.Trainer(
         logger=logger if with_wandb else None,
