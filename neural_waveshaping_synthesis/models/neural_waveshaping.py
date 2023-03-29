ad8959f450da00c75b6df530b7c97c6a8210231b
Pratik Sutar <sutarpratik2012@gmail.com>
2023-03-30 01:37:15 +0900

    development

--- neural_waveshaping_synthesis/models/neural_waveshaping.py
+++ neural_waveshaping_synthesis/models/neural_waveshaping.py
@@ -122,12 +122,15 @@ class NeuralWaveshaping(pl.LightningModule):
         print(f"\n\nIn forward")
         print(f"f0: {f0.shape}")
         print(f"control: {control.shape}")
+        print(f"f0: {f0[0,0,:10].detach().cpu().numpy()}")
 
         f0_upsampled = F.upsample(f0, f0.shape[-1] * self.control_hop, mode="linear") # f0.shape[-1] is number of frames
         print(f"f0_upsampled: {f0_upsampled.shape}")
+        print(f"f0_upsampled: {f0_upsampled[0,0,:10].detach().cpu().numpy()}")
 
         x = self.render_exciter(f0_upsampled)
         print(f"x: {x.shape}")
+        print(f"x: {x[0,0,:10].detach().cpu().numpy()}")
 
         control_embedding, gru_embedding = self.get_embedding(control)
 
