814a2de5e1dbcf933f7681d648000c4912b4f811
Pratik Sutar <sutarpratik2012@gmail.com>
2023-03-31 16:34:39 +0900

    editing

--- neural_waveshaping_synthesis/models/neural_waveshaping.py
+++ neural_waveshaping_synthesis/models/neural_waveshaping.py
@@ -253,12 +253,13 @@ class NeuralWaveshaping(pl.LightningModule):
         print(f"x: {x[0,0,:10].detach().cpu().numpy()}")
 
         control_embedding, z = self.get_embedding(control)
-        return control_embedding, z
-    def decode(self, control_embedding, z):
         print(f"control_embedding: {control_embedding[0,:10,0].detach().cpu().numpy()}")
         print(f"control_embedding: {control_embedding[0,:10,1].detach().cpu().numpy()}")
         print(f"control_embedding: {control_embedding[0,:10,2].detach().cpu().numpy()}")
         print(f"control_embedding: {control_embedding[0,:10,3].detach().cpu().numpy()}")
+        return control_embedding, z
+    def decode(self, control_embedding, z):
+
 
         print(f"\nInvoking NEWT with x and control_embedding")
         x = self.newt(x, control_embedding)
