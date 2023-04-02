7e75fea1948c83d66ae5c4598b38a1e38bd9fa4d
Pratik Sutar <sutarpratik2012@gmail.com>
2023-04-02 23:51:25 +0900

    debugging

--- neural_waveshaping_synthesis/models/neural_waveshaping.py
+++ neural_waveshaping_synthesis/models/neural_waveshaping.py
@@ -187,7 +187,7 @@ class NeuralWaveshaping(pl.LightningModule):
 
         self.sample_rate = sample_rate
 
-        self.embedding = ControlModule()
+        self.embedding = ControlModule(z_dim=z_dim)
 
         self.osc = HarmonicOscillator()
         self.harmonic_mixer = nn.Conv1d(self.osc.n_harmonics, n_waveshapers, 1)
