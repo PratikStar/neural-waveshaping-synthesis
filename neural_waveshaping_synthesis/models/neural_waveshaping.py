22c33b4a0bffee0b4ddb56a2ab65a7297ad18a83
Pratik Sutar <sutarpratik2012@gmail.com>
2023-04-02 13:48:26 +0900

    development

--- neural_waveshaping_synthesis/models/neural_waveshaping.py
+++ neural_waveshaping_synthesis/models/neural_waveshaping.py
@@ -131,7 +131,7 @@ class ControlModule(nn.Module):
         print(f"After Cond1D: {y.shape}")
         return y, x
 
-    def get_control_from_z(self, controls, z):
+    def _get_control_from_z(self, controls, z):
         print(f"\nRunning get_control_from_z")
         print(f"Input to get_control_from_z: {controls.shape}, {z.shape}")
         if self.embedding_strategy == "STATIC_DYNAMIC_Z":
