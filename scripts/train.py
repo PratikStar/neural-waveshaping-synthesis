116ee8b64791bacd6743c273419d48d640a407b7
Pratik Sutar <sutarpratik2012@gmail.com>
2023-04-03 11:47:28 +0900

    commit & push

--- scripts/train.py
+++ scripts/train.py
@@ -10,7 +10,7 @@ from neural_waveshaping_synthesis.models.neural_waveshaping import NeuralWavesha
 
 
 @gin.configurable
-def get_model(model, with_wandb, hidden_size=None):
+def get_model(model, with_wandb, hidden_size=16):
     return model(log_audio=with_wandb, hidden_size=hidden_size)
 
 
