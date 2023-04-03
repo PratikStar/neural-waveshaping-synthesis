51ccab400c7c7dad8efd9f03ddc22f4a1693583d
Pratik Sutar <sutarpratik2012@gmail.com>
2023-04-03 16:32:51 +0900

    commit & push

--- scripts/train.py
+++ scripts/train.py
@@ -61,7 +61,7 @@ def main(
                    )
         config = dict(wandb.config)
         print(config)
-        checkpoint_path = f"/root/nws/timbre-16k-f0_di_{config['confidence_threshold']}-static_dynamic_z_{config['hidden_size']//2}_{config['hidden_size']//2}"
+        checkpoint_path = f"/root/nws/timbre-16k-f0_di_{config['confidence_threshold']}-static_dynamic_z_{config['z'][0]}_{config['z'][1]}"
 
         model = get_model(with_wandb=with_wandb, hidden_size=config['hidden_size'])
     else:
