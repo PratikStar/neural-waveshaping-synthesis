b9df657b01f4eb476c9c94a0f6dfc3b1f0eef0b5
Pratik Sutar <sutarpratik2012@gmail.com>
2023-04-05 03:15:48 +0900

    development

--- wandb_artifact_upload.py
+++ wandb_artifact_upload.py
@@ -4,5 +4,8 @@ import os
 path = "/root/nws"
 
 wandb.init(project="nws", name="artifact_update")
-for os
+for f in os.listdir():
+    if not f.startswith("timbre"):
+        continue
+    artifact = wandb.Artifact()
 
