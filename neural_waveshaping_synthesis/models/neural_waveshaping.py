2a1b59d2a5e1154e31ceceb6e07c6a9075463bb5
Pratik Sutar <sutarpratik2012@gmail.com>
2023-04-08 00:46:51 +0900

    editing

--- neural_waveshaping_synthesis/models/neural_waveshaping.py
+++ neural_waveshaping_synthesis/models/neural_waveshaping.py
@@ -223,27 +223,6 @@ class ControlModule(nn.Module):
 
             return y, x # NOTE, because I need "x"
 
-            z_dynamic, _ = self.gru(controls.transpose(1, 2))
-            print(f"After GRU (z_dynamic): {z_dynamic.shape}")
-            print(z_dynamic[0,0,:10].detach().cpu().numpy())
-            print(z_dynamic[0,1,:10].detach().cpu().numpy())
-
-            # static
-            # stack = []
-            # for i in range(len(presets)):
-            #     stack.append(self.timbre_z[presets[i]])
-            z_static = torch.stack([z_static])
-            # print(f"after stacking: {z_static.shape}")
-
-            z_static = z_static.unsqueeze(1).repeat(1, self.sample_rate // self.control_hop, 1)
-            print(f"z_static after repeat: {z_static.shape}")
-            print(z_static[0,0,:10].detach().cpu().numpy())
-            print(z_static[0,1,:10].detach().cpu().numpy())
-
-            x = torch.cat((z_dynamic, z_static), 2)
-            print(f"After cat: {x.shape}")
-            print(x[0,0,:100].detach().cpu().numpy())
-            print(x[0,1,:100].detach().cpu().numpy())
         else:
             pass
 
