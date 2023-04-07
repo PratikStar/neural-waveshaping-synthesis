9a54379263b7251ae40b8bd26c906ed151bae87b
Pratik Sutar <sutarpratik2012@gmail.com>
2023-04-07 18:54:21 +0900

    editing

--- neural_waveshaping_synthesis/models/neural_waveshaping.py
+++ neural_waveshaping_synthesis/models/neural_waveshaping.py
@@ -66,14 +66,16 @@ class ControlModule(nn.Module):
             self.proj = nn.Conv1d(self.hidden_size, embedding_size, 1)
         elif self.embedding_strategy == "CONCAT_STATIC_Z":
             # dynamic
-            self.timbre_z = {}
-            device = 'cuda' if torch.cuda.device_count() > 0 else 'cpu'
-            for i in range(21):
-                for a in ['A', 'B', 'C', 'D']:
-                    self.timbre_z[f"{i:02d}{a}"] = torch.nn.Parameter(torch.randn(self.z_static_size, device=device), requires_grad=True)
+            self.embed = nn.Embedding(80, self.z_static_size)
+
+            # self.timbre_z = {}
+            # device = 'cuda' if torch.cuda.device_count() > 0 else 'cpu'
+            # for i in range(21):
+            #     for a in ['A', 'B', 'C', 'D']:
+            #         self.timbre_z[f"{i:02d}{a}"] = torch.nn.Parameter(torch.randn(self.z_static_size, device=device), requires_grad=True)
 
             print("Init: Here is the embedding table")
-            print(self.timbre_z)
+            print(self.embed)
             self.gru = nn.GRU(control_size, self.z_dynamic_size, batch_first=True)
             # static
             self.proj = nn.Conv1d(self.hidden_size, embedding_size, 1)
