6c1bd0a722971fedcbe7bbfc3965d1b6ec04f16a
Pratik Sutar <sutarpratik2012@gmail.com>
2023-04-07 18:54:28 +0900

    development

--- neural_waveshaping_synthesis/models/neural_waveshaping.py
+++ neural_waveshaping_synthesis/models/neural_waveshaping.py
@@ -73,7 +73,6 @@ class ControlModule(nn.Module):
             # for i in range(21):
             #     for a in ['A', 'B', 'C', 'D']:
             #         self.timbre_z[f"{i:02d}{a}"] = torch.nn.Parameter(torch.randn(self.z_static_size, device=device), requires_grad=True)
-
             print("Init: Here is the embedding table")
             print(self.embed)
             self.gru = nn.GRU(control_size, self.z_dynamic_size, batch_first=True)
