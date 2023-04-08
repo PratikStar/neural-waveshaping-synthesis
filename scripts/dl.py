3db0c9bf290b784e2e823df2cdedb239d24c92cc
Pratik Sutar <sutarpratik2012@gmail.com>
2023-04-08 15:14:28 +0900

    debugging

---
+++ scripts/dl.py
@@ -0,0 +1,16 @@
+from neural_waveshaping_synthesis.data.general import GeneralDataModule
+
+dm = GeneralDataModule(
+    "/root/data/nws/timbre-16k-f0_di_75_batch",
+    load_to_memory=False,
+    num_workers=1,
+    shuffle=False,
+    batch_size=3
+)
+dm.setup(stage="fit")
+dl = dm.train_dataloader()
+print(len(dl))
+
+it = iter(dl)
+batch = next(it)
+print(batch['name'])
