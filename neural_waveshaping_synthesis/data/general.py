6d47d86c3348cf2fb0c215f9846009295354bd3a
Pratik Sutar <sutarpratik2012@gmail.com>
2023-04-08 14:45:38 +0900

    debugging

--- neural_waveshaping_synthesis/data/general.py
+++ neural_waveshaping_synthesis/data/general.py
@@ -91,7 +91,8 @@ class MyBatchSampler(Sampler):
         self.content_enc = [1,2,3,4]
     def __iter__(self):
         cnt = random.choice(self.content_enc)
-        preset = random.sample(range(20), 3)
+        preset = random.sample(range(20), self.batch_size)
+        
         for batch in self.batches:
             yield batch
 
